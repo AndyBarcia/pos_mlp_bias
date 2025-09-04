c26329ea11b985e39e13e4a00e34d6720bcdacec
AndyBarcia <andybarcia4@gmail.com>
2025-09-04 20:00:54 +0200

    Profiling script with ncu

---
+++ profile.sh
@@ -0,0 +1,8 @@
+sudo -E "PATH=$PATH" "PYTHONPATH=$PYTHONPATH" \
+    /usr/local/cuda/bin/ncu \
+    -o report_backward \
+    --target-processes all \
+    --set full \
+    -k pos_mlp_bias_backward_kernel_optimized \
+    -f python3 test.py
+
